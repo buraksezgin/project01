7344f7e7b48a5c6ffd194e2267b845affad0af9f
buraksezgin <73567330+buraksezgin@users.noreply.github.com>
2020-11-08 11:08:55 -0600

    the fouth commit from local repo

--- src/gitProject/FirstClass.java
+++ src/gitProject/FirstClass.java
@@ -7,7 +7,7 @@ public class FirstClass {
 		System.out.println("Hi");
 		System.out.println("Third line of code I am typing");
 		System.out.println("The first line of code I am typing from remote repo");
-		
+		System.out.println("Here is a merge conflict I create from local master");
 	}
 
 }
